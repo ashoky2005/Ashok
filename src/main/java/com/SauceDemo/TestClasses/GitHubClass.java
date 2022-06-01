56364ae456dc833db00585a7091d0cfe320fbab0
Admin <Admin@DESKTOP-TN5BT5Q>
2022-06-01 13:29:42 +0530

    new Git hub Class is created

---
+++ src/main/java/com/SauceDemo/TestClasses/GitHubClass.java
@@ -0,0 +1,12 @@
+package com.SauceDemo.TestClasses;
+import org.testng.annotations.Test;
+
+public class GitHubClass {
+	
+	@Test
+	public void method1()
+	{
+		System.out.println("Git Hub Class");
+	}
+
+}
